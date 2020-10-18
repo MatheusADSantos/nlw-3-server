a385798476f2541a57f03b90e8bccd11b9e0dfe8
MatheusADSantos <matheusdevmobile2020@gmail.com>
2020-10-18 12:26:16 -0300

    Validating datas...

--- src/controllers/OrphanagesControllers.ts
+++ src/controllers/OrphanagesControllers.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import orphanageView from '../views/orphanages_view';
+import * as Yup from 'yup';
 
 import Orphanage from '../models/Orphanage';
 
 export default {
   async index(request: Request, response: Response) {
     const orphanagesRepository = getRepository(Orphanage);
-    
+
     const orphanages = await orphanagesRepository.find({
       relations: ['images']
     });
@@ -45,7 +46,7 @@ export default {
       return { path: image.filename }
     })
 
-    const orphanage = orphanagesRepository.create({
+    const data = {
       name,
       latitude,
       longitude,
@@ -54,7 +55,28 @@ export default {
       opening_hours,
       open_on_weekends,
       images
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      )
     });
+
+    await schema.validate(data, {
+      abortEarly: false,
+    });
+
+    const orphanage = orphanagesRepository.create(data);
 
 
 
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express, { json } from 'express';
 import path from 'path';
+import cors from 'cors';
+
 import 'express-async-errors';
 
 import './database/connection';
@@ -8,6 +10,12 @@ import errorHandler from './errors/handler';
 import routes from './routes';
 const app = express();
 
+//qual a origem do frontend que nossa api permitiria acessar
+// app.use(cors({
+//   origin: 'localhost://3000/'
+// }));
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
